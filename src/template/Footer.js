2605a6678dc5a3ce20ab419af853e85b158e2945
Dickson Murithi <dicksonmurithi155@gmail.com>
2025-02-24 16:32:21 +0300

    addition

--- src/template/Footer.js
+++ src/template/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="mt-auto py-5 bg-dark">
       <div className="container d-flex justify-content-center">
-        <span className="text-white">Copyright &copy; Website 2024</span>
+        <span className="text-white">Copyright &copy; Website 2025</span>
       </div>
     </footer>
   );
